32d785bb539f03d353ec1715f1d5e19eecab870e
Sam Lee <sammystradling2006@gmail.com>
2022-06-14 17:13:50 +0100

    added findlowest temp function => yet to be implemenmted

--- tempchecker.py
+++ tempchecker.py
@@ -8,3 +8,14 @@ def find_highest_temp(cityTemps):
             maxCity = location[0]
     #print(f"The hottest city is {maxCity} at {maxTemp} degrees")
     return maxCity, maxTemp
+
+
+def find_lowest_temp(cityTemps):
+    minTemp = cityTemps[0][1]
+    minCity = ""
+    for location in cityTemps:
+        if location[1] < minTemp:
+            minTemp = location[1]
+            minCity = location[0]
+    #print(f"The hottest city is {maxCity} at {maxTemp} degrees")
+    return minCity, minTemp
