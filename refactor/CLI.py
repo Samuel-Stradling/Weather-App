2feb8cf50efa88713eb60577fbea4a6959c2e723
Sam Lee <sammystradling2006@gmail.com>
2023-06-02 17:23:01 +0100

    refactor folder to rewrite the code much more clearly and concisely

---
+++ refactor/CLI.py
@@ -0,0 +1,19 @@
+import re
+from datahandling import processTime, hottestCity, hottestTemp, coolestCity, coolestTemp
+
+def format_city_name(city_name):
+    if "+" in city_name:
+        city_name = city_name.replace("+", " ")
+
+    match = re.search(r"(.+)(?=, GB)", city_name)
+    if match:
+        city_name = match.group(1)
+    return city_name
+
+print(f"process[{processTime}s]")
+
+print(f"The hottest city was {format_city_name(hottestCity)} at {hottestTemp}°C")
+print()
+print(f"The coolest city was {format_city_name(coolestCity)} at {coolestTemp}°C")
+
+
---
+++ refactor/datahandling.py
@@ -0,0 +1,83 @@
+import requests
+from cities import cities as citiesTuple # tuple containing england cities
+import time
+
+tic = time.gmtime() # starts the timer for the process 
+
+
+# API KEY DIRECTORY, MAY NEED TO CHANGE DEPENDING ON WHERE IT IS STORED
+APIKeyDirectory = "key.txt"
+
+
+with open(APIKeyDirectory, 'r') as keyFile:
+    api_key = keyFile.readline()
+
+
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+
+def get_temp(city_name):
+
+    """Returns the temperature of a given city.
+    
+    If the argument 'city_name' is not found in the API repsonse, raises ValueError
+
+    Parameters
+    -------------
+    city_name : str
+        The name of the city in England to get the temperature of
+
+    Raises
+    -------------
+    ValueError
+        If the city_name is not found in the 
+        
+    """
+
+    # complete url with city name
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+
+    # get method of requests module
+    response = requests.get(complete_url)
+
+    # json response object; convert to python format data
+    responseJson = response.json()
+
+    # x contains llist of dictionaries
+    # if cod = 404, city is not found
+    if responseJson["cod"] != "404":
+
+        weatherData = responseJson["main"]
+        # main contains another dictionary witht he main temperature for that city
+
+        current_temperature = weatherData['temp']
+    else:
+        raise ValueError("city not found")
+    return round((current_temperature - 273.15), 2)
+
+
+
+
+
+
+cityTemps = {}
+for city in citiesTuple:
+    try:
+        cityTemps[city] = get_temp(city)
+    except ValueError:
+        print(f"No data found for {city}")
+        continue
+
+
+
+hottestCity = max(cityTemps, key=cityTemps.get)
+hottestTemp = cityTemps[hottestCity]
+
+coolestCity = min(cityTemps, key=cityTemps.get)
+coolestTemp = cityTemps[coolestCity]
+
+
+toc = time.gmtime() # ends the timer for the process
+
+processTime = abs(tic[5] - toc[5])
